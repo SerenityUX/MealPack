a799126c6dd0dde970c2d9ccb3a89c690fe0f6e4
SerenityUX <thomas@serenidad.app>
2023-07-08 12:29:23 -0400

    Initial Commit

---
+++ MealPack/MealPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPackApp.swift
+//  MealPack
+//
+//  Created by Thomas Stubblefield on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MealPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
